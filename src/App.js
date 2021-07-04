f7725f98cca5eb173af5b25a4c8cb0bde49c3528
nazmulm <naz05haq@gmail.com>
2021-07-05 00:50:10 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React.js
-        </a>
-      </header>
+import React, {Component} from 'react'
+class App extends Component{
+  state = {
+    name : "naveen",
+    age : 23,
+    occupation : "student"
+  }
+  render(){
+    return <div>
+      <p>I am a man named {this.state.name}</p>
+      <p>I am a man with age {this.state.age}</p>
+      <p>I am a man named {this.state.occupation}</p>
+      <button>Click me</button>
     </div>
-  );
+  }
 }
-
 export default App;
